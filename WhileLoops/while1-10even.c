2dde24a44668682967d4654b183b3335869e79ba
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-04 20:37:37 +0530

    Add files via upload

---
+++ WhileLoops/while1-10even.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+
+
+{
+    int x = 1;
+     while (x <= 10)
+     {
+        /* code */
+        if (x %2 == 0)
+        {
+            printf("%d \n",x);
+        }
+
+        x++;
+        
+     }
+     
+}
---
+++ WhileLoops/while1-10evensum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{
+    int x = 1;
+    int s = 0;
+
+    while(x <= 10)
+    {
+        if (x % 2 == 0)
+        {
+            s = s + x;
+        }
+         x++;
+        
+    }
+    printf("%d \n",s);
+}
