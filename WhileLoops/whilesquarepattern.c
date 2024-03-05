fb1a4c191b733f61a6a1b8e47781cf449960f62e
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-05 21:52:49 +0530

    Add files via upload

---
+++ WhileLoops/whilesquarepattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main ()
+{
+    int r = 1;
+    
+    while (r <= 5)
+    {
+        /* code */
+
+      int c = 1;
+        while (c <= 5)
+        {  
+            c++;
+            printf("%d \t",c);
+        }
+        r++;
+        printf("\n");
+        
+    }
+    
+
+}
