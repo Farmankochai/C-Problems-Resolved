c26e0641e24c7e9193a4d31302740987a7f5fbcf
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-09 20:37:30 +0530

    Add files via upload

---
+++ ForLoops/07for2-10evennoandsum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    int s = 0;
+    for ( i = 2; i <= 10; i=i+2)
+    {
+        /* code */
+        s = s + i;
+    }
+    printf(" sum is %d",s);
+    
+}
