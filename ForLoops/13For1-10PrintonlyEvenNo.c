c73e12c254c8105479ec6dc366d09867d9acb33f
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-13 22:27:23 +0530

    Add files via upload

---
+++ ForLoops/13For1-10PrintonlyEvenNo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main ()
+{
+    int i;
+    for ( i = 1; i <= 10; i++)
+    {
+        /* code */
+        if (i % 2 == 0)
+        {
+            /* code */
+            printf("%d ",i);
+        }
+        
+    }
+    
+}
