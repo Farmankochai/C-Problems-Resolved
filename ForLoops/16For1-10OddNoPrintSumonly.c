38b927b8e22c617a963ddaaea0a6513f83857ccf
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-13 22:28:57 +0530

    Add files via upload

---
+++ ForLoops/16For1-10OddNoPrintSumonly.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{
+    int i;
+    int sum = 0;
+    for ( i = 0; i <= 10; i++)
+    {
+        /* code */
+        if (i % 2!=0)
+        {
+            /* code */
+        }
+        sum = sum + i;
+    }
+    
+    printf("%d ",sum);
+}
