cf918b4b842d781c65504b0c513e9d666622852f
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-14 23:31:19 +0530

    Add files via upload

---
+++ ForLoops/17ForSquarePattern.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+void main()
+{
+   int r;
+   int c;
+    
+    for ( r = 1; r <= 5; r++)
+    {
+        /* code */
+        for ( c = 1; c <= 5; c++)
+        {
+            /* code */
+            printf("%d \t",c);
+        }
+        printf("\n");
+        
+    }
+    
+}
