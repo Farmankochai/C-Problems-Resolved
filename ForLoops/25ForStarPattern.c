a9dbe64cffe6f61b1338173f67730235201f553d
CodeWithMac <139049875+Farmankochai@users.noreply.github.com>
2024-03-16 22:48:59 +0530

    Add files via upload

---
+++ ForLoops/25ForStarPattern.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main ()
+{
+    int r;
+    int c;
+    int s;
+
+    for (r=1; r<=5; r++)
+    {
+        for(s=r; s<5; s++)
+        {
+            printf("  ");
+        }
+        for(c=1; c<=r; c++)
+        {
+            printf("* ");
+        }
+        for(c=1; c< r; c++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
