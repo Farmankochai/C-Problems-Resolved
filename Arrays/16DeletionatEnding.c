a484db5ed0cd16a9599efe593cd12988edcb85fd
CodewithFarman <139049875+Farmankochai@users.noreply.github.com>
2024-03-24 21:37:30 +0530

    Add files via upload

---
+++ Arrays/16DeletionatEnding.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main ()
+{
+    int arr[6];
+    int i;
+    int num;
+
+    for ( i = 0; i < 6; i++)
+    {
+        /* code */
+        printf("Enter Values: ");
+        scanf("%d",&num);
+        arr[i] = num;
+    }
+
+    
+
+    for ( i = 0; i < 5; i++)
+    {
+        /* code */
+        printf("%d ",arr[i]);
+    }
+    
+}
